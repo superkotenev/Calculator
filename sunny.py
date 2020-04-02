96dc369b9c1b72d2b8d7ff3c6db36697812aae29
superkotenev <kotenev.matvei@gmail.com>
2020-04-02 20:31:45 +0200

    Added the combination function

--- sunny.py
+++ sunny.py
@@ -5,4 +5,12 @@ def fact(a):
 		a -= 1
 	return ans
 
-print(fact(int(input())))
+def comb(n,k):
+	if n < k:
+		print("n must bigger then k or equal to it ")
+		return False 
+	elif n == k:
+		return 1
+	else:
+		ans = fact(n)/(fact(n-k)*fact(k))
+		return ans
