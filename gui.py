b8ddff71144a3a6db116b74caf834eecc6807f38
superkotenev <kotenev.matvei@gmail.com>
2020-04-05 00:19:33 +0200

    Added a simple lambda function with entry.get

--- gui.py
+++ gui.py
@@ -4,8 +4,8 @@ from PIL import ImageTk, Image
 HEIGHT = 500
 WIDTH = 600
 
-def test():
-    print("Button clicked")
+def test(entry):
+    print("Button clicked", entry)
 
 root = tk.Tk()
 
@@ -22,13 +22,13 @@ frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
 entry = tk.Entry(frame, bg="white", bd=5)
 entry.place(relx=0, rely=0, relwidth=0.6, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", bg="#417cab", fg="white", activebackground="#2b6e5e", command=test)
+button = tk.Button(frame, text="Get Weather", bg="#417cab", fg="white", activebackground="#2b6e5e", command=lambda: test(entry.get()))
 button.place(relx=0.7, rely=0, relwidth=0.3, relheight=1)
 
 lower_frame = tk.Frame(root, bg="#2b6e5e", bd=5)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
 
-label = tk.Label(lower_frame, text="This is a label", bg="white",)
+label = tk.Label(lower_frame, text="Text label", bg="white")
 label.place(relwidth=1, relheight=1)
 
 root.mainloop()
